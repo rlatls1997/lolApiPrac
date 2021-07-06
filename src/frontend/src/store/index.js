79555791fb6502ea3db24743783dafdab87d7e33
sinhyuk <rlatls1997@gmail.com>
2021-07-06 19:28:08 +0900

    feat: vuex, axios 추가

---
+++ src/frontend/src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import lol from "./modules/lol";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {},
+    mutations: {},
+    actions: {},
+    modules: {lol},
+});
